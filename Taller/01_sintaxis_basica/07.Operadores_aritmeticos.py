6c050fd265d31314faa67564345b453c7d60a306
Juan José Ruiz Cruz <juanjo_rc31@hotmail.com>
2022-11-06 10:37:42 -0600

    Se agregaron líneas de código
    Se agregaron líneas de código de ejemplo al archivo Operadores_aritmeticos.py

--- Taller/01_sintaxis_basica/07.Operadores_aritmeticos.py
+++ Taller/01_sintaxis_basica/07.Operadores_aritmeticos.py
@@ -1,3 +1,27 @@
 #suma o adición
 suma = 2 + 2
 print(suma)
+
+#resta o sustracción
+resta = 4-1
+print(resta)
+
+#multiplicación 
+multi = 2*2
+print(multi)
+
+#división
+divi = 4/2
+print(divi)
+
+#modulo
+modulo = 4%2
+print(modulo)
+
+#potencia
+poten = 2**2
+print(poten)
+
+#división entera
+divint = 4//2
+print(divint)
